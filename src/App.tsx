b21c1d3242d7a9b415ed3eb143ee53b4d5bd7698
Varady Zsolt (fubenalvo) <fubenalvo1987@gmail.com>
2026-08-07 09:32:27 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div>
+      <h1>Wheel of Fortune</h1>
+      <img src={heroImg} className="base" width="170" height="179" alt="" />
+              <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+    </div>
+   
+  )
+}
+
+export default App
